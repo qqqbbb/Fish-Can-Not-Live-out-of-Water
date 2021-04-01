9dba93b2238abab235e17c5fea9c59fe86e49eff
qqqbbb <qqqinbox@ya.ru>
2021-04-01 09:50:19 +0300

    1

---
+++ Config.cs
@@ -0,0 +1,16 @@
+﻿using SMLHelper.V2.Json;
+using SMLHelper.V2.Options.Attributes;
+
+
+namespace Fish_Out_Of_Water
+{
+    [Menu("Fish Can Not Live Out Of Water")]
+    public class Config : ConfigFile
+    {
+        [Slider("Time fish lives out of water", 1, 1000, DefaultValue = 180, Step = 1, Format = "{0:F0}", Tooltip = "This is ingame, not real time seconds. 1 ingame second is 1 real time second at time scale 1")]
+        public int outOfWaterLiveTime = 180;
+        //[Toggle("Logging")]
+        //public bool logging = false;
+
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,65 @@
+﻿
+using HarmonyLib;
+using QModManager.API.ModLoading;
+using System.Reflection;
+using System;
+using SMLHelper.V2.Handlers;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Fish_Out_Of_Water
+{
+    [QModCore]
+    public class Main
+    {
+        internal static Config config { get; } = OptionsPanelHandler.RegisterModOptions<Config>();
+        static bool gameLoaded = false;
+        public static PDA pda;
+
+        public static void Log(string str, QModManager.Utility.Logger.Level lvl = QModManager.Utility.Logger.Level.Info)
+        {
+            QModManager.Utility.Logger.Log(lvl, str);
+        }
+
+        [HarmonyPatch(typeof(Player), "TrackTravelStats")]
+        class Player_TrackTravelStats_patch
+        { // runs when game finished loading
+            public static void Postfix(Player __instance)
+            {
+                if (!gameLoaded)
+                {
+                    Player.main.isUnderwaterForSwimming.changedEvent.AddHandler(Player.main, new UWE.Event<Utils.MonitoredValue<bool>>.HandleFunction(Fish_Out_Of_Water.OnPlayerIsUnderwaterForSwimmingChanged));
+                    pda = Player.main.GetPDA();
+                    Fish_Out_Of_Water.AddFishToList();
+                    gameLoaded = true;
+                }
+                //ErrorMessage.AddDebug(" TrackTravelStats ");
+            }
+        }
+
+        [HarmonyPatch(typeof(IngameMenu), "QuitGameAsync")]
+        internal class IngameMenu_QuitGameAsync_Patch
+        {
+            public static void Postfix(IngameMenu __instance, bool quitToDesktop)
+            {
+                //ErrorMessage.AddDebug("QuitGameAsync " + quitToDesktop);
+                if (!quitToDesktop)
+                {
+                    gameLoaded = false;
+                    Fish_Out_Of_Water.fishOutOfWater = new Dictionary<LiveMixin, float>();
+                }
+
+            }
+        }
+
+        [QModPatch]
+        public static void Load()
+        {
+
+            config.Load();
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            new Harmony($"qqqbbb_{assembly.GetName().Name}").PatchAll(assembly);
+        }
+    }
+}
